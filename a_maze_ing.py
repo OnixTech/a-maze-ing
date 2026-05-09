f6e3de317cc13529419db748e34452e700a9c7cc
Luciano Petillo <luciano.prnl@gmail.com>
2026-05-09 16:51:16 +0200

    mazegenerator

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,10 +1,37 @@
-import sys
+class Cell:
+    def __init__(self) -> None:
+        self.walls = 0b1111
+        self.visited = False
+
+class MazeGenerator:
+    def __init__(self, width: int, height: int) -> None:
+        self.width = width
+        self.height = height
+        self.grid = []
+
+    def create_grid(self) -> None:
+        for _ in range(self.height):
+            row = []
+
+            for _ in range(self.width):
+                row.append(Cell())
+
+            self.grid.append(row)
+
+    def export(self, filename: str) -> None:
+        with open(filename, "w") as file:
+            for row in self.grid:
+                for cell in row:
+                    file.write(f"{cell.walls:X}")
+
+                file.write("\n")
+
+
 
 def main() -> None:
-    with open(sys.argv[1], "r") as file:
-        content = file.read()
-
-    print(content)
+    maze = MazeGenerator(10,10)
+    maze.create_grid()
+    maze.export("maze.txt")
 
 
 if __name__ == "__main__":
